0dba019538e6c6ff1c8edf352a79a800a61bebdf
suryansh <suryanshpatwal@gmail.com>
2024-08-16 14:29:23 +0530

    cheek tapak damdam

--- server/index.js
+++ server/index.js
@@ -1,21 +1,23 @@
 const express = require('express');
-const bodyPraser = require('body-praser');
-const { server, socket } = require('socket.io');
+const bodyParser = require('body-parser');
+const { Server } = require('socket.io');
 
-const io = new server();
 const app = express();
+const httpServer = require('http').createServer(app); // Create an HTTP server
 
-app.use(bodyPraser.json());
-const emailToSocketMapping = new Map;
+const io = new Server(httpServer);
 
+
+app.use(bodyParser.json());
+const emailToSocketMapping = new Map(); // Assuming you meant to use 'emailToSocketMapping'
 
 io.on("connection", (socket) => {
-    socket.on('join-room', data => {
-        console.log('User', EmailId, 'joined room', roomId)
-        emailToSocketMapping.set(EmailId, socket.id);
-        const { roomId, EmailId } = data
+    socket.on('join-room', (data, emailId) => { // Option 2: separate argument for emailId
+        console.log('User', emailId, 'joined room', data.roomId) // Using data.roomId
+        emailToSocketMapping.set(emailId, socket.id);
+        const { roomId } = data; // Option 1: destructure roomId from data
         socket.join(roomId);
-        socket.broadcast.to(roomId).emit('user-joined', { EmailId })
+        socket.broadcast.to(roomId).emit('user-joined', { userId: emailId }); // Consider using userId instead
     });
 });
 
